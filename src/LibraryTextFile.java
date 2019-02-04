f6135f36817a52c6e20af290f1f03409fcf9f819
kyledexperience <kyledexperience@gmail.com>
2019-02-04 11:43:47 -0500

    yeah

--- src/LibraryTextFile.java
+++ src/LibraryTextFile.java
@@ -5,30 +5,42 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 public class LibraryTextFile {
 
-	private static Path filePath = Paths.get("Library.txt");
+	private static Path filePath = Paths.get("NonFiction.txt");
+	private static Path filePath2 = Paths.get("Fiction.txt");
 
 	public static void appendToFile(Library library) throws IOException {
 		if (Files.notExists(filePath)) {
 			Files.createFile(filePath);
 		}
-		
+
 		String library1 = library.getTitle() + ", " + library.getAuthor();
 		List<String> libraryList = Arrays.asList(library1);
-		
+
 		Files.write(filePath, libraryList, StandardOpenOption.APPEND);
-		
-		
+
 	}
 
 	public static List<Library> readFile() throws IOException {
 		List<String> lines = Files.readAllLines(filePath);
 		List<Library> library = new ArrayList<>();
-		
+
+		for (String line : lines) {
+			String[] parts = line.split(",");
+			Library c = new Library(parts[0], (parts[1]));
+			library.add(c);
+		}
+		return library;
+
+	}
+
+	public static List<Library> readFile2() throws IOException {
+		List<String> lines = Files.readAllLines(filePath2);
+		List<Library> library = new ArrayList<>();
+
 		for (String line : lines) {
 			String[] parts = line.split(",");
 			Library c = new Library(parts[0], (parts[1]));
